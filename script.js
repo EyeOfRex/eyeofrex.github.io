a1086cdc7eb0e47541b0d5dd57077e8067f531ae
Eye of rex <eye_of_rex@hotmail.com>
2025-01-02 17:43:14 +0100

    add gif on pages, add 404 page and other...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggle = document.getElementById("themeToggle");
-    const root = document.documentElement; // Permet d'accéder aux variables CSS définies dans :root
+    const root = document.documentElement; 
 
     let isDarkTheme = localStorage.getItem("isDarkTheme");
     if (isDarkTheme === null) {
@@ -9,35 +9,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
 
-    // Fonction pour mettre à jour le thème
+    
     const updateTheme = () => {
         if (isDarkTheme === "false") {
-            // Passer au thème clair
+            // go to light theme
             root.style.setProperty("--background-dark", "#fdebf3");
             root.style.setProperty("--text-light", "#1e1e2e");
             root.style.setProperty("--background-light", "#1e1e2e");
             root.style.setProperty("--text-dark", "#fdebf3");
-            themeToggle.src = "/res/sun.png"; // Icône du soleil pour le thème clair
+            themeToggle.src = "/res/sun.png"; 
             localStorage.setItem("isDarkTheme", "false");
         } else if (isDarkTheme === "true") {
-            // Passer au thème sombre
+            // go to dark theme
             root.style.setProperty("--background-dark", "#1e1e2e");
             root.style.setProperty("--text-light", "#fdebf3");
             root.style.setProperty("--background-light", "#fdebf3");
             root.style.setProperty("--text-dark", "#1e1e2e");
-            themeToggle.src = "/res/moon.svg"; // Icône de la lune pour le thème sombre
+            themeToggle.src = "/res/moon.svg";
             localStorage.setItem("isDarkTheme", "true");
         }
     };
 
-    // Ajouter un gestionnaire d'événement pour basculer le thème lors du clic
+    
     themeToggle.addEventListener("click", () => {
         isDarkTheme = isDarkTheme === "true" ? "false" : "true";
         localStorage.setItem("isDarkTheme", isDarkTheme);
-        updateTheme(); // Appliquer le nouveau thème
+        updateTheme();
     });
 
-    // Initialiser le thème sombre par défaut
+    
     updateTheme();
 
    
